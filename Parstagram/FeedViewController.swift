0cebac336d962774dca71793b509a162f32bc28e
Jose Patino <jose.patino88@icloud.com>
2020-02-28 11:23:13 -0500

    Creating Fake Comments

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -72,6 +72,30 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         }
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        let post = posts[indexPath.row]
+        let comment = PFObject(className: "Comments")
+        
+        comment["text"] = "This is a random comment"
+        comment["posts"] = post
+        comment["author"] = PFUser.current()!
+        
+        post.add(comment, forKey: "comments")
+        
+        post.saveInBackground { (success, error) in
+            if success
+            {
+                print("Comment saved")
+            }
+            else
+            {
+                print("Error saving comment: \(error)")
+            }
+        }
+        
+    }
+    
     /*
     // MARK: - Navigation
 
